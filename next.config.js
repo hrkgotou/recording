39d2d9697cddcdc6bd4560ac90c0f28666fcd6e2
hrkgotou <hrkgotou@gmail.com>
2022-09-06 12:38:33 +0900

    [fix]GitHub Actions 周りを修正

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   optimizeFonts: false,
+  basePath: process.env.GITHUB_ACTIONS && '/recording',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
